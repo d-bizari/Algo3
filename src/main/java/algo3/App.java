2a387fc34f8852fd66e2ca8c3c7b1830e63736d7
azulzaietz <azulzaietz@gmail.com>
2019-11-23 22:25:12 -0300

    Cambio a un ChoiceBox la seleccion del sector en la scene de configuracion

--- src/main/java/algo3/App.java
+++ src/main/java/algo3/App.java
@@ -6,6 +6,7 @@ import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -57,25 +58,16 @@ public class App extends Application {
         grid.setVgap(8);
         grid.setHgap(10);
 
-        //Para recibir nombres jugadores y sectores seleccionados
+        //Para recibir nombres jugadores y sectores1 seleccionados
 
         //Jugador 1
         Label nombreJugador1 = new Label("Nombre:");
         GridPane.setConstraints(nombreJugador1, 5, 0);
-        //Input jugador 1
+        //Nombre ingresado
         TextField nombreIngresado1 = new TextField();
         nombreIngresado1.setPromptText("Ingrese su nombre");
         nombreIngresado1.setEditable(true);
         GridPane.setConstraints(nombreIngresado1, 6, 0);
-        //Eleccion de sector
-        Label sectorJugador1 = new Label("Sector:");
-        GridPane.setConstraints(sectorJugador1, 5, 1);
-        //Sector ingresado
-        TextField sectorIngresado1 = new TextField();
-        sectorIngresado1.setEditable(true);
-        sectorIngresado1.setPromptText("1 o 2");
-        GridPane.setConstraints(sectorIngresado1, 6, 1);
-
         //Jugador 2
         Label nombreJugador2 = new Label("Nombre:");
         GridPane.setConstraints(nombreJugador2, 5, 4);
@@ -84,22 +76,36 @@ public class App extends Application {
         nombreIngresado2.setPromptText("Ingrese su nombre");
         nombreIngresado2.setEditable(true);
         GridPane.setConstraints(nombreIngresado2, 6, 4);
+
         //Eleccion de sector
+        ChoiceBox<Integer> sectores1 = new ChoiceBox<Integer>();
+        sectores1.getItems().add(1);
+        sectores1.getItems().add(2);
+        sectores1.setValue(1);
+        //Jugador 1
+        Label sectorJugador1 = new Label("Sector:");
+        GridPane.setConstraints(sectorJugador1, 5, 1);
+        //Sector ingresado
+        GridPane.setConstraints(sectores1, 6, 1);
+
+        //Eleccion de sector
+        ChoiceBox<Integer> sectores2 = new ChoiceBox<Integer>();
+        sectores2.getItems().add(1);
+        sectores2.getItems().add(2);
+        sectores2.setValue(1);
+        //Jugador 2
         Label sectorJugador2 = new Label("Sector:");
         GridPane.setConstraints(sectorJugador2, 5, 5);
         //Sector ingresado
-        TextField sectorIngresado2 = new TextField();
-        sectorIngresado2.setEditable(true);
-        sectorIngresado2.setPromptText("1 o 2");
-        GridPane.setConstraints(sectorIngresado2, 6, 5);
+        GridPane.setConstraints(sectores2, 6, 5);
 
         //Play
         Button play = new Button("Comenzar a jugar!");
-        play.setOnAction(e -> { juego.agregarJugador(nombreIngresado1.getText(), sectorIngresado1.getText()); juego.agregarJugador(nombreIngresado2.getText(), sectorIngresado2.getText());});
+        play.setOnAction(e -> { juego.agregarJugador(nombreIngresado1.getText(), sectores1.getValue()); juego.agregarJugador(nombreIngresado2.getText(), sectores2.getValue());});
         GridPane.setConstraints(play, 10, 10);
 
         //Add everything to grid
-        grid.getChildren().addAll(nombreJugador2, nombreIngresado2, sectorJugador2, sectorIngresado2, play, nombreJugador1, nombreIngresado1, sectorJugador1, sectorIngresado1);
+        grid.getChildren().addAll(nombreJugador1, nombreIngresado1, nombreJugador2, nombreIngresado2, sectores1, sectores2, play);
 
         Scene scene2 = new Scene(grid, 2000, 2000);
         return scene2;
