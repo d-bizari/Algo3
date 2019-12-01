78453fabc456f36900a41dc6a7aaa9a7f563e52a
valeriafb <valeriafboch@gmail.com>
2019-12-01 20:02:23 -0300

    Creo clase controlador de boton mover

--- src/main/java/algo3/OpcionesDeJuego.java
+++ src/main/java/algo3/OpcionesDeJuego.java
@@ -1,5 +1,7 @@
 package algo3;
 
+import Controlador.BotonMoverUnidad;
+import Modelo.AlgoChess;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
@@ -10,15 +12,17 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.TextAlignment;
 
-public class OpcionesDeJuego extends VBox {
-
+public class OpcionesDeJuego<TableroGridPanetablero> {
+    VBox caja;
+    AlgoChess juego;
+    String jugador;
+    TableroGridPane tablero;
     public OpcionesDeJuego() {
-        super();
-
+        this.caja = new VBox();
         this.ubicarTitulo();
-        this.setAlignment(Pos.TOP_CENTER);
-        this.setSpacing(20);
-        this.setPadding(new Insets(25));
+        caja.setAlignment(Pos.TOP_CENTER);
+        caja.setSpacing(20);
+        caja.setPadding(new Insets(25));
 
         this.fijarBotones();
 
@@ -30,10 +34,12 @@ public class OpcionesDeJuego extends VBox {
         titulo.setFont(Font.font("Tahoma", FontWeight.BOLD, 18));
         titulo.setTextAlignment(TextAlignment.CENTER);
         titulo.setTextFill(Color.web("000000"));
-        this.getChildren().addAll(titulo);
+        caja.getChildren().addAll(titulo);
     }
 
     public void fijarBotones() {
+        BotonMoverUnidad botonMoverUnidad = new BotonMoverUnidad(juego, tablero, jugador);
+
         Button botonMover = new Button();
         botonMover.setText("Mover");
         botonMover.setStyle("-fx-base: #ff763d;");
@@ -44,7 +50,7 @@ public class OpcionesDeJuego extends VBox {
         botonAtacar.setStyle("-fx-base: #ff763d;");
         botonAtacar.setPrefSize(120, 30);
 
-        this.getChildren().addAll(botonMover, botonAtacar);
+        caja.getChildren().addAll(botonMover, botonAtacar);
 
     }
 }
